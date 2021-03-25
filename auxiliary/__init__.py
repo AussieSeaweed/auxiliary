a36f69abbab6cabde45fa09b6b09ee5561fa0e10
Juho <juho-kim@outlook.com>
2021-03-25 18:44:19 -0400

    Avoiding recursive calls

--- auxiliary/__init__.py
+++ auxiliary/__init__.py
@@ -1,9 +1,10 @@
 from auxiliary.enums import OrderedEnum
+from auxiliary.iterables import (after, chunked, const, flattened, iter_equal, product, rotated, sum_, trimmed, unique,
+                                 windowed)
 from auxiliary.tests import ExtendedTestCase
 from auxiliary.typing import SupportsAdd, SupportsLessThan, SupportsMul
-from auxiliary.utils import (after, bind, chunked, const, default, flattened, get, iter_equal, next_or_none, product,
-                             rotated, sum_, trimmed, unique, windowed)
+from auxiliary.utils import bind, default, get, next_or_none
 
-__all__ = ('OrderedEnum', 'ExtendedTestCase', 'SupportsAdd', 'SupportsLessThan', 'SupportsMul', 'after', 'bind',
-           'chunked', 'const', 'default', 'flattened', 'get', 'iter_equal', 'next_or_none', 'product', 'rotated',
-           'sum_', 'trimmed', 'unique', 'windowed')
+__all__ = ('OrderedEnum', 'after', 'chunked', 'const', 'flattened', 'iter_equal', 'product', 'rotated', 'sum_',
+           'trimmed', 'unique', 'windowed', 'ExtendedTestCase', 'SupportsAdd', 'SupportsLessThan', 'SupportsMul',
+           'bind', 'default', 'get', 'next_or_none')
