13b82bb949ac8168c8ccc5d81da9e7677a01cde9
Juho <juho-kim@outlook.com>
2021-03-25 16:18:49 -0400

    Added SupportsAdd to __init__

--- auxiliary/__init__.py
+++ auxiliary/__init__.py
@@ -1,9 +1,9 @@
 from auxiliary.enums import OrderedEnum
 from auxiliary.tests import ExtendedTestCase
-from auxiliary.typing import SupportsLessThan, SupportsMul
+from auxiliary.typing import SupportsAdd, SupportsLessThan, SupportsMul
 from auxiliary.utils import (after, bind, chunked, const, default, flattened, get, iter_equal, next_or_none, product,
                              rotated, sum_, trimmed, unique, windowed)
 
-__all__ = ('OrderedEnum', 'ExtendedTestCase', 'SupportsLessThan', 'SupportsMul', 'after', 'bind', 'chunked', 'const',
-           'default', 'flattened', 'get', 'iter_equal', 'next_or_none', 'product', 'rotated', 'sum_', 'trimmed',
-           'unique', 'windowed')
+__all__ = ('OrderedEnum', 'ExtendedTestCase', 'SupportsAdd', 'SupportsLessThan', 'SupportsMul', 'after', 'bind',
+           'chunked', 'const', 'default', 'flattened', 'get', 'iter_equal', 'next_or_none', 'product', 'rotated',
+           'sum_', 'trimmed', 'unique', 'windowed')
