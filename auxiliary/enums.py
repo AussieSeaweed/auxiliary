fd28c79ca15dfded8c0eb39128bc46ab975b99e6
Juho <juho-kim@outlook.com>
2021-06-01 21:01:16 -0400

    reformatted file

--- auxiliary/enums.py
+++ auxiliary/enums.py
@@ -12,6 +12,12 @@ class OrderedEnum(Enum):
     They can be compared with others compared by their indices.
     """
 
+    def __lt__(self: _OE_co, other: _OE_co) -> bool:
+        if isinstance(other, type(self)):
+            return self.index < other.index
+        else:
+            return NotImplemented
+
     @cached_property
     def index(self) -> int:
         """Returns the ordinal of this ordered enum element.
@@ -21,12 +27,6 @@ class OrderedEnum(Enum):
         values = list[OrderedEnum](type(self))
 
         return values.index(self)
-
-    def __lt__(self: _OE_co, other: _OE_co) -> bool:
-        if isinstance(other, type(self)):
-            return self.index < other.index
-        else:
-            return NotImplemented
 
 
 _OE_co = TypeVar('_OE_co', bound=OrderedEnum)
